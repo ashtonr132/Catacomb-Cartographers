20267b37ddcf5424bc1f0bc765270da0f3d295b7
rob ashton <ashtonr132@gmail.com>
2019-03-03 17:27:38 +0000

    camera and player moves

--- Assets/Scripts/CameraStuff.cs
+++ Assets/Scripts/CameraStuff.cs
@@ -7,7 +7,7 @@ public class CameraStuff : MonoBehaviour
 {
     [SerializeField]
     internal GameObject player;
-    internal float cameraDist = 3.5f;
+    internal float cameraDist = 3.5f, camSpeed = 2;
 
     // Update is called once per frame
     void Update()
@@ -23,12 +23,17 @@ public class CameraStuff : MonoBehaviour
                 try
                 {
                     Vector3 position = transform.position;
-                    int levelSize = LevelGen.levelSize / 2, cap = 5;
-                    position.x = Mathf.Clamp(Mathf.Lerp(transform.position.x, player.transform.position.x, Time.deltaTime * Vector3.Distance(transform.position, player.transform.position) / 2), -levelSize + cap, levelSize - cap);
+                    int levelSize = LevelGen.levelSize / 2;
+                    position.x = Mathf.Clamp(Mathf.SmoothStep(transform.position.x, player.transform.position.x, Time.deltaTime * Vector3.Distance(transform.position, player.transform.position) / 2), -levelSize, levelSize);
                     position.y = player.transform.position.y + cameraDist * 2;
-                    position.z = Mathf.Clamp(Mathf.Lerp(transform.position.z, player.transform.position.z - 1, Time.deltaTime * Vector3.Distance(transform.position, player.transform.position) / 2), -levelSize + cap, levelSize - cap);
+                    position.z = Mathf.Clamp(Mathf.SmoothStep(transform.position.z, player.transform.position.z, Time.deltaTime * Vector3.Distance(transform.position, player.transform.position) / 2), -levelSize, levelSize);
                     transform.position = position;
-                    transform.rotation = Quaternion.LookRotation(player.transform.forward, Vector3.up);
+                    Vector3 rotation = transform.rotation.eulerAngles;
+                    Vector3 lr = player.transform.rotation.eulerAngles;
+                    rotation.x = Mathf.Lerp(rotation.x, lr.x, Time.deltaTime * camSpeed);
+                    rotation.y = Mathf.Lerp(rotation.y, lr.y, Time.deltaTime * camSpeed);
+                    rotation.z = Mathf.Lerp(rotation.z, lr.z, Time.deltaTime * camSpeed);
+                    transform.rotation = Quaternion.Euler(rotation);
                 }
                 catch
                 {
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -7,20 +7,53 @@ public class PlayerControls : MonoBehaviour
     Rigidbody rb;
     float playerspeed = 2;
     GameObject menu;
+    Animator an;
+
 	// Use this for initialization
 	void Start ()
     {
         menu = GameObject.Find("Menu");
         rb = transform.GetComponent<Rigidbody>();
+        an = transform.GetComponent<Animator>();
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
-        rb.velocity = new Vector3(Input.GetAxis("Horizontal") * playerspeed, 0, Input.GetAxis("Vertical") * playerspeed);
+        if (Mathf.Abs(rb.velocity.x) < 0.2f)
+        {
+            an.SetBool("Moving", false);
+            an.SetBool("Idle", true);
+        }
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             menu.GetComponent<Menu>().inGameSettings();
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            an.SetBool("Moving", true);
+            an.SetBool("Idle", false);
+            var sc = transform.localScale;
+            sc.x *= sc.x > 0 ? -1 : 1;
+            transform.localScale = sc;
+            rb.velocity = -transform.right *playerspeed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            an.SetBool("Moving", true);
+            an.SetBool("Idle", false);
+            var sc = transform.localScale;
+            sc.x *= sc.x > 0 ? 1 : -1;
+            transform.localScale = sc;
+            rb.velocity = transform.right * playerspeed;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(Vector3.forward * playerspeed);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(-Vector3.forward * playerspeed);
         }
     }
 }
